28070da72d7e7d071b2913b3a19a0c68244b9672
Blanche Cérémonie <130890116+Surtalaevateinn@users.noreply.github.com>
2025-11-29 21:49:30 +0100

    Add files via upload

---
+++ src/main/java/com/isep/ffa/mapper/AnnouncementMapper.java
@@ -0,0 +1,8 @@
+package com.isep.ffa.mapper;
+
+import com.isep.ffa.entity.Announcement;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AnnouncementMapper extends CustomBaseMapper<Announcement> {
+}
